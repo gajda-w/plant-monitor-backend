b4e9226aa23d8646e43b029d45d95325b34e3d2e
Wojciech Gajda <wojciech.gajda@mirumee.com>
2024-11-17 17:18:34 +0100

    typos

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 
 function getRandomSensorData() {
-    const temperature = (Math.random() * (30-15) + 15).toFixed(1);
-    const humadity = (Math.random() * (100 - 40) + 40).toFixed(1);
-    return {temperature, humadity}
+  const temperature = (Math.random() * (30 - 15) + 15).toFixed(1);
+  const humidity = (Math.random() * (100 - 40) + 40).toFixed(1);
+  return { temperature, humidity };
 }
 
-app.get('/api/sensors', (req, res) => {
-    const sensorData = getRandomSensorData();
-    res.json(sensorData);
+app.get("/api/sensors", (req, res) => {
+  const sensorData = getRandomSensorData();
+  res.json(sensorData);
 });
 
-app.get('/', (req, res) => {
-    res.send('Welcome on server')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome on the Plant Monitor server");
+});
 
-app.listen(port,'0.0.0.0', () => {
-    console.log(`server works on http://localhost:${port}`)
-})
+app.listen(port, "0.0.0.0", () => {
+  console.log(`server is working http://localhost:${port}`);
+});
