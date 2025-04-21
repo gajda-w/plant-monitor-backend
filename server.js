747d2ad5045dcb9a816020b3ead3329096557cb5
Wojciech Gajda <wojciech.gajda@mirumee.com>
2025-04-21 18:10:39 +0200

    add light hours

--- server.js
+++ server.js
@@ -8,13 +8,21 @@ const port = 4000;
 const sensorType = 22;
 const gpioPin = 4;
 
+let lightStartHour = 6;
+let lightEndHour = 23;
+
 app.use(
   cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET'],
-    allowHeaders: ['Content-Type', 'ngrok-skip-browser-warning'],
+    origin: [
+      'http://localhost:3000',
+      'https://plant-monitor-frontend-rouge.vercel.app/',
+    ],
+    methods: ['GET', 'POST'], // <--- DODAJ POST
+    allowedHeaders: ['Content-Type', 'ngrok-skip-browser-warning'],
   })
 );
+
+app.use(express.json()); // <--- TO JEST KLUCZOWE!
 
 async function readSensor() {
   try {
@@ -25,15 +33,13 @@ async function readSensor() {
     };
   } catch (err) {
     console.error('Failed to read sensor data:', err);
+    return null;
   }
 }
 
-const startLightTime = 6;
-const endLightTime = 24;
-
 const getLightStatus = () => {
   const currentHour = new Date().getHours();
-  return currentHour >= startLightTime && currentHour < endLightTime;
+  return currentHour >= lightStartHour && currentHour < lightEndHour;
 };
 
 app.get('/api/status', async (req, res) => {
@@ -44,6 +50,21 @@ app.get('/api/status', async (req, res) => {
     isHumidifierOn: true,
   };
   res.json(sensorData);
+});
+
+app.post('/api/light-hours', (req, res) => {
+  const { startHour, endHour } = req.body;
+  if (
+    typeof startHour === 'number' &&
+    typeof endHour === 'number' &&
+    startHour >= 0 &&
+    endHour <= 24
+  ) {
+    lightStartHour = startHour;
+    lightEndHour = endHour;
+    return res.json({ success: true });
+  }
+  return res.status(400).json({ success: false, message: 'Invalid hours' });
 });
 
 app.get('/', (req, res) => {
