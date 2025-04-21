03228baf975456b16197709f10cd1f3fc2bf205e
Wojciech Gajda <wojciech.gajda@mirumee.com>
2025-04-21 14:24:52 +0200

    feat: get real data from the DTH sensor

--- server.js
+++ server.js
@@ -1,22 +1,42 @@
-const express = require("express");
+const sensor = require('node-dht-sensor').promises;
+const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 4000;
 
-function getRandomSensorData() {
-  const temperature = (Math.random() * (30 - 15) + 15).toFixed(1);
-  const humidity = (Math.random() * (100 - 40) + 40).toFixed(1);
-  return { temperature, humidity };
+const sensorType = 22;
+const gpioPin = 4;
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET'],
+    allowHeaders: ['Content-Type', 'ngrok-skip-browser-warning'],
+  })
+);
+
+async function readSensor() {
+  try {
+    const { temperature, humidity } = await sensor.read(sensorType, gpioPin);
+    return {
+      temperature: temperature.toFixed(1),
+      humidity: humidity.toFixed(1),
+    };
+  } catch (err) {
+    console.error('Failed to read sensor data:', err);
+  }
 }
 
-app.get("/api/sensors", (req, res) => {
-  const sensorData = getRandomSensorData();
+app.get('/api/sensors', async (req, res) => {
+  const sensorData = await readSensor();
   res.json(sensorData);
 });
 
-app.get("/", (req, res) => {
-  res.send("Welcome on the Plant Monitor server");
+app.get('/', (req, res) => {
+  res.send('Welcome on server');
 });
 
-app.listen(port, "0.0.0.0", () => {
-  console.log(`server is working http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`server works on http://localhost:${port}`);
 });
